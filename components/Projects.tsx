23f57e8a98d2fcf4ae1f9e72e7c4bb8efb08e1fa
Aaditya Malani <aadityamalanii15@gmail.com>
2024-09-05 21:08:37 +0530

    changes

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -9,8 +9,8 @@ const Projects = () => {
   return (
     <div className="py-20">
       <h1 className="heading">
-        A small selection of{" "}
-        <span className="text-purple">recent projects</span>
+      A Showcase of Our {" "}
+        <span className="text-purple">Latest Endeavors</span>
       </h1>
       <div className="flex flex-wrap items-center justify-center p-4 gap-16 mt-10">
         {projects.map((item) => (
