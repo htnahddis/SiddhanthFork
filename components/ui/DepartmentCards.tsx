9332c55b8fad079da7c003e37db500f49843d346
Receipes App <student.siddhanthchapade@gmail.com>
2024-10-30 00:57:13 +0530

    12_3_Images & DE Section

--- components/ui/DepartmentCards.tsx
+++ components/ui/DepartmentCards.tsx
@@ -131,18 +131,14 @@ const DataEngineeringContent = () => {
           >
             <p className="text-neutral-600 dark:text-neutral-400 text-base md:text-2xl font-sans max-w-3xl mx-auto">
               <span className="font-bold text-neutral-700 dark:text-neutral-200">
-                Teach Machines to Learn.
+              Converting data into actionable insights.
               </span>{" "}
-              Machine learning is revolutionizing industries by enabling systems
-              to learn from data. Explore algorithms such as decision trees,
-              neural networks, and deep learning models. Whether its building
-              predictive models or implementing AI-powered solutions, you will
-              gain hands-on experience with the most sought-after skills in tech
-              today.
+              Data engineering is reshaping the way organizations manage and utilize their data. By designing robust data pipelines and architectures, data engineers ensure that data flows seamlessly from various sources to analytical tools. Explore essential technologies like ETL processes, data warehousing, and big data frameworks. From building scalable data infrastructures to optimizing data storage and retrieval,
+              you will gain practical experience in the critical skills needed to drive data-driven decision-making in today's tech landscape.
             </p>
             <Image
               src="https://assets.aceternity.com/macbook.png"
-              alt="Machine Learning"
+              alt="Data Engineering"
               height="500"
               width="500"
               className="md:w-1/2 md:h-1/2 h-full w-full mx-auto object-contain"
@@ -159,29 +155,29 @@ const data = [
     category: "Web Development",
     title: "You can build with us.",
     // src: "https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    src: "",
+    src: "/SiddhanthChanges/web development.webp",
     content: <WebDevelopmentContent />,
   },
   {
     category: "Data Analysis",
     title: "Enhance your data analytics skills.",
     // src: "https://images.unsplash.com/photo-1666875753105-c63a6f3bdc86?q=80&w=2946&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-   src: "",
+   src: "/SiddhanthChanges/analytics.jpg",
     content: <DataAnalysisContent />,
   },
   {
     category: "Machine Learning",
     title: "We train and deploy models.",
     // src: "https://images.unsplash.com/photo-1561557944-6e7860d1a7eb?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    src: "",
+    src: "/SiddhanthChanges/MachineLearning.webp",
     content: <MachineLearningContent />,
   },
 
   {
-    category: "Machine Learning",
-    title: "We train and deploy models.",
+    category: "Data Engineering",
+    title: "Converting data into actionable insights.",
     // src: "https://images.unsplash.com/photo-1561557944-6e7860d1a7eb?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    src: "",
+    src: "/SiddhanthChanges/dataEngineering.avif",
     content: <DataEngineeringContent />,
   },
 ];
